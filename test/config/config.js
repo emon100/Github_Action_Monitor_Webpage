a185e301e54f2e04e7cb1798bcb71f19a275390b
emon100 <emon100@qq.com>
2020-02-23 22:18:16 +0800

    Version 4.

--- test/config/config.js
+++ test/config/config.js
@@ -1,4 +1,4 @@
-const https = require('https');
+const superagent = require('superagent');
 const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'
 };
@@ -6,25 +6,29 @@ const sitesConfig = {
     '东大教务处官网': {
         //type有html和json两种。
         type: "html",
-        protocol: "http",
-        siteHost: "http://aao.neu.edu.cn/",
+        siteURL: "http://aao.neu.edu.cn/",
         parts: {
             '教学研究': {
+                maxLength: 10,
                 //cheerio的dom元素选择器语法，类似jQuery的选择器和CSS Selector语法
-                selector: ['[frag="窗口9"] li:first-child']
+                selector: '[frag="窗口9"] li'
             }
         }
     },
     '计算机学院官网': {
         type: "html",
-        protocol: "http",
-        siteHost: "http://www.cse.neu.edu.cn/",
+        siteURL: "http://www.cse.neu.edu.cn/",
         parts: {
             '通知公告': {
+                maxLength: 10,
                 //自己定义的json反序列化之后的对象的处理函数，输出字符串。
-                processor: function ($) {
-                    let result = $('[frag="窗口76"] .con .news_list li:first-child span a').text() + ' ';
-                    result += $('[frag="窗口76"] .con .news_list li:first-child span:last-child').text();
+                processor: function (dom) {
+                    let domList = dom.window.document.querySelectorAll('[frag="窗口76"] .con .news_list li');
+                    let result = [];
+
+                    for (const node of domList) {
+                        result.push(`${node.children[0].children[0].textContent} ${node.children[1].textContent}`);
+                    }
                     return result;
                 }
             }
@@ -32,160 +36,121 @@ const sitesConfig = {
     },
     '热榜': {
         type: 'json',
-        protocol: 'https',
-        siteHost: 'https://www.tophub.fun:8888/v2/GetAllInfoGzip?id=59&page=0',
+        siteURL: 'https://www.tophub.fun:8888/v2/GetAllInfoGzip?id=59&page=0',
         parts: {
-            '第一个': {
+            'V2ex': {
                 //自己定义的json反序列化之后的对象的处理器，输出字符串。
+                maxLength: 10,//下面processor也有
                 processor: function (obj) {
-                    let result;
-                    if (obj != null && obj.Code === 0) {
-                        result = `${unescape(obj['Data']['data'][0]['Title'])} : ${unescape(obj['Data']['data'][0]['hotDesc'])}`;
-                    } else {
-                        result = 'api访问错误';
+                    let result= [];
+                    if (obj != null && obj["Code"] === 0){
+                        for(let i=0;i<10;++i){
+                            result.push(`${unescape(obj['Data']['data'][i]['Title'])} : ${unescape(obj['Data']['data'][i]['hotDesc'])}`);
+                        }
                     }
                     return result;
                 }
             }
         }
     },
+    /*
     '东大BB': {
         type: "json",
-        getResponse: function () {
+        getResponse: async function () {
             //POST发送用户名，密码https://bb.neu.edu.cn/webapps/login/，
             //拿到session_id, s_session_id
             //拿去访问https://bb.neu.edu.cn/webapps/blackboard/execute/announcement?method=search&context=mybb&viewChoice=2
             //访问两次才有结果
-            return new Promise((resolve, reject) => {
-                let postData1 = `user_id=${process.env.STUDENT_ID}&password=${process.env.BB_PASSWORD}&x=${Math.floor(Math.random() * 43)}&y=${Math.floor(Math.random() * 17)}&action=login&remote-user=&new_loc=&auth_type=&one_time_token=%E5%BE%85%E5%AE%9A%E5%86%85%E5%AE%B9&encoded_pw=&encoded_pw_unicode=`;
-                let newHeaders = {};
-                Object.assign(newHeaders, headers);
+            let newHeaders = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
+                'Host': `bb.neu.edu.cn`,
+                'Origin': `https://bb.neu.edu.cn`,
+                'Connection': 'keep-alive'
+            };
 
-                newHeaders['Content-Type'] = 'application/x-www-form-urlencoded';
-                newHeaders['Content-Length'] = Buffer.byteLength(postData1);
-                let request1 = https.request(`https://bb.neu.edu.cn/webapps/login/`, {
-                        method: 'POST',
-                        headers: newHeaders,
-                        rejectUnauthorized: false,
-                        timeout: 30000
-                    },
-                    res1 => {
-                        //第一步
-                        const encoding = res1.headers['content-encoding'];
-                        if (encoding === 'undefined') {
-                            res1.setEncoding('utf-8');
-                        }
-                        console.log(`Step1 code: ${res1.statusCode}`);
+            let postData1 = {
+                user_id: '20184366',
+                password: '518518',
+                x: `${Math.floor(Math.random() * 43)}`,
+                y: `${Math.floor(Math.random() * 17)}`,
+                action: 'login',
+                one_time_token: '待定内容'
+            };
 
-                        newHeaders.Cookie = res1.headers["set-cookie"];
+            let postData2 = {
+                cmd: 'loadStream',
+                streamName: 'alerts',
+                providers: '{}',
+                forOverview: 'false'
+            };
 
-                        newHeaders['Host'] = 'bb.neu.edu.cn';
+            const agent = superagent.agent();
 
-                        //console.log(newHeaders.Cookie);
+            agent.set(newHeaders);
+            agent.timeout({response: 30000});
+            let a = null;
+            try {
+                await  agent.get('https://bb.neu.edu.cn/');
 
-                        //第二步
-                        let postData2 = 'cmd=loadStream&streamName=alerts&providers=%7B%7D&forOverview=false';
-                        newHeaders['Content-Type'] = 'application/x-www-form-urlencoded';
-                        newHeaders['Content-Length'] = Buffer.byteLength(postData2);
-                        let request2 = https.request('https://bb.neu.edu.cn/webapps/streamViewer/streamViewer', {
-                                method: 'POST',
-                                headers: newHeaders,
-                                rejectUnauthorized: false,
-                                timeout: 30000
-                            },
-                            res2 => {
-                                console.log(`Step2 code: ${res2.statusCode}`);
-                                const encoding = res2.headers['content-encoding'];
-                                if (encoding === 'undefined') {
-                                    res2.setEncoding('utf-8');
-                                }
+                agent.type('form');
 
-                                newHeaders.Cookie = res2.headers["set-cookie"];
+                await agent
+                    .post('https://bb.neu.edu.cn/webapps/login/')
+                    .send(postData1);
 
-                                //console.log(newHeaders.Cookie);
+                await agent.get('https://bb.neu.edu.cn/webapps/bb-social-learning-BBLEARN/execute/mybb?cmd=display&toolId=AlertsOnMyBb_____AlertsTool');
 
-                                //第三步
-                                let request3 = https.request('https://bb.neu.edu.cn/webapps/streamViewer/streamViewer', {
-                                        method: 'POST',
-                                        headers: newHeaders,
-                                        rejectUnauthorized: false,
-                                        timeout: 30000
-                                    },
-                                    res3 => {
-                                        console.log(`Step3 code: ${res3.statusCode}`);
-                                        const encoding = res3.headers['content-encoding'];
-                                        if (encoding === 'undefined') {
-                                            res3.setEncoding('utf-8');
-                                        }
-                                        let json = '';
-                                        res3.on('data', chunk => {
-                                            json += chunk;
-                                        });
 
-                                        res3.on('end', () => {
-                                            console.log("BB step3 over");
-                                            resolve(json);
-                                        });
+                await agent.get('https://bb.neu.edu.cn/webapps/streamViewer/streamViewer?cmd=view&streamName=alerts&globalNavigation=false');
 
-                                        res3.on('error', (err) => {
-                                            reject('step3:' + err);
-                                        })
+                agent.set({'Referer':'https://bb.neu.edu.cn/webapps/streamViewer/streamViewer?cmd=view&streamName=alerts&globalNavigation=false',
+                    'Sec-Fetch-Dest': 'empty',
+                    'Sec-Fetch-Mode': 'cors',
+                    'X-Prototype-Version': 1.7,
+                    'X-Requested-With': 'XMLHttpRequest'});
 
-                                    });
+                a = await agent
+                    .post('https://bb.neu.edu.cn/webapps/streamViewer/streamViewer')
+                    .send(postData2);
 
-                                request3.on("error", err => {
-                                    reject("BB step3 error "+err);
-                                });
-                                request3.write(postData2);
-                                request3.end();
-
-                                res2.on('end', () => {
-                                    console.log('BB step2 over');
-
-                                });
-
-                                res2.on('error', (err) => {
-                                    console.log('step2: ' + err);
-                                })
-                            });
-                        request2.write(postData2);
-                        request2.end();
-
-                        request2.on('error', (error) => {
-                            reject('BB: Step2 error '+ error);
-                        });
-
-                        res1.on('end', () => {
-                            console.log('BB step1 logged in.');
-                        });
-                    });
-
-                request1.write(postData1);
-                request1.on('error', (e) => {
-                    reject('BB: Step1 error '+e);
-                });
-                request1.end();
-            });
+                let count =5;
+                while(JSON.parse(a.text)['sv_streamEntries'].findIndex(v=>v['providerId']==='bb-nautilus')===-1&&count){
+                    a = await agent
+                        .post('https://bb.neu.edu.cn/webapps/streamViewer/streamViewer')
+                        .send(postData2);
+                    count--;
+                }
+                a = a.text;
+            } catch (e) {
+                console.log(e);
+                a = 'Get BB failed'
+            }
+            console.log(a);
+            return a;
         },
         parts: {
             '最新课程公告': {
+                maxLength: 10,
                 processor: function (obj) {
                     if (obj != null && obj["sv_streamEntries"].length >= 1) {
                         //找最新日期
-                        let newest_se_id = "bb-nautilus" + obj["sv_providers"].find(v => {
+                        let newest_id =  obj["sv_providers"].find(v => {
                             return v["sp_provider"] === "bb-nautilus";
                         })["sp_newest"];
 
-                        if (newest_se_id === -1) {
-                            return "Don't exist";
+                        if (newest_id === -1) {
+                            throw "Don't exist newest_se_id";
                         }
+
+                        let newest_se_id = "bb-nautilus"+newest_id.toString();
 
                         let newest_obj = obj["sv_streamEntries"].find(v => {
                             return v["se_id"] === newest_se_id;
                         });
 
+
                         if (newest_obj == null) {
-                            return "can't found";
+                            throw "can't found newest_obj";
                         }
 
                         let newest_obj_course_id=newest_obj["se_courseId"];
@@ -194,11 +159,13 @@ const sitesConfig = {
                         return `${course_name} 中的 ${newest_obj["itemSpecificData"]["title"]}更新 \n ${newest_obj["se_details"]}`;
 
                     }
-                    return "BB JSON not completed.";
+                    throw "BB JSON not completed.";
                 }
             }
         }
     }
+    */
+
 };
 
 module.exports.config = sitesConfig;
